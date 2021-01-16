c60e0bc87610550a2dfb260464d34ccda5a14e07
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-01-16 09:45:05 -0700

    addidng files to models and routes folders.  Updated server.js

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// Exporting an object which contains all of our models
+
+module.exports = {
+    Workout: require("./workout")
+  };
--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require("express");
-const path = require("path");
+const logger = require("morgan")
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-// const exerciseRoutes = require("./routes/exercises");
-// const workoutRoutes = require("./routes/workouts");
-const apiRoutes = require("./routes/api");
-const viewRoutes = require("./routes/views");
 
+// setting up Express App
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3008;
+
+app.use(logger("dev"));
+
+  // Sets up the Express app for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Sets up db mongo
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+// Creating Routes
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
+});
