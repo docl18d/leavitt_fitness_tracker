5c07fe95f230c00b3cd350227c8d3cd1238b1e23
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-01-19 17:42:48 -0700

    fixing routing issues

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Server Dependencies
 const express = require("express");
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const path = require('path');
 
 // Database Connection Request
@@ -14,8 +14,8 @@ const db = require("./models");
 let app = express();
 
 // Added so body parser can handle post requests
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Host Static Files so css and js files can be retrieved
 app.use(express.static(path.join(__dirname, '/public')));
@@ -24,88 +24,89 @@ app.use(express.static(path.join(__dirname, '/public')));
 let PORT = process.env.PORT || 3008;
 
 //Set up routes and endpoints//
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+// app.get("/", (req,res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-app.get("/", (req,res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get("/exercise", (req,res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
+// });
 
-app.get("/exercise", (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
-});
+// app.get("/stats", (req,res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+// });
 
-app.get("/stats", (req,res) => {
-  res.sendFile(path.join(__dirname, 'public', 'stats.html'));
-});
+// //Middleware//
 
-//Middleware//
-
-//GET Requests
-
-
-app.get("/api/workouts", (req,res) => {
-  db.Workout.find({}).sort({day:-1}).limit(1)
-  .then(dbWorkout => {
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.json(err);
-  });
-});
-
-app.get("/api/workouts/range", (req,res) => {
-  db.Workout.find({})
-  .then(dbWorkout => {
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.json(err);
-  });
-});
+// //GET Requests
 
 
+// app.get("/api/workouts", (req,res) => {
+//   db.Workout.find({}).sort({day:-1}).limit(1)
+//   .then(dbWorkout => {
+//     res.json(dbWorkout);
+//   })
+//   .catch(err => {
+//     res.json(err);
+//   });
+// });
 
-//PUT Requests
-
-app.put("/api/workouts/:id", (req,res) => {
-
-let urlData = req.params;
-let data = req.body;
-  db.Workout.updateOne( {_id: urlData.id }, {$push: {exercises:  [
-    {
-    "type" : data.type,
-    "name" : data.name,
-    "duration" : data.duration,
-    "distance" : data.distance,
-    "weight" : data.weight,
-    "reps" : data.reps,
-    "sets" : data.sets
-    }
-  ] 
-}}).then(dbUpdate => {
-  res.json(dbUpdate);
-})
-.catch(err => {
-  res.json(err);
-});
-
-});
+// app.get("/api/workouts/range", (req,res) => {
+//   db.Workout.find({})
+//   .then(dbWorkout => {
+//     res.json(dbWorkout);
+//   })
+//   .catch(err => {
+//     res.json(err);
+//   });
+// });
 
 
-//POST Requests
 
-app.post("/api/workouts", (req,res) => {
+// //PUT Requests
 
-  let data = req.body;
+// app.put("/api/workouts/:id", (req,res) => {
 
-  db.Workout.create({
-    day: new Date().setDate(new Date().getDate())
-}).then(dbUpdate => {
-      res.json(dbUpdate);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+// let urlData = req.params;
+// let data = req.body;
+//   db.Workout.updateOne( {_id: urlData.id }, {$push: {exercises:  [
+//     {
+//     "type" : data.type,
+//     "name" : data.name,
+//     "duration" : data.duration,
+//     "distance" : data.distance,
+//     "weight" : data.weight,
+//     "reps" : data.reps,
+//     "sets" : data.sets
+//     }
+//   ] 
+// }}).then(dbUpdate => {
+//   res.json(dbUpdate);
+// })
+// .catch(err => {
+//   res.json(err);
+// });
+
+// });
+
+
+// //POST Requests
+
+// app.post("/api/workouts", (req,res) => {
+
+//   let data = req.body;
+
+//   db.Workout.create({
+//     day: new Date().setDate(new Date().getDate())
+// }).then(dbUpdate => {
+//       res.json(dbUpdate);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
 // Starts the server to begin listening
 app.listen(PORT, function(){
