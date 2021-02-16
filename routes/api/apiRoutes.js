09de11a67016c81526924e68b6f780a4d25f4c5b
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-02-16 14:27:30 -0700

    fixing routing error

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,53 +1,50 @@
-const router = require('express').Router();
-const { Workout } = require('../../models')
-
+const router = require("express").Router();
+const { Workout } = require("../../models")
 
 router.get("/workouts", (req, res) => {
     Workout.find({})
-    .then(dbWorkout => {
-        console.log("data", dbWorkout)
-        res.json(dbWorkout);
-    })
-    .catch(err => {
-        res.status(400).json(err)
-    })
-})
+        // .sort({ date: -1 })
+        .then(dbWorkout => {
+            console.log("data", dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 router.put("/workouts/:id", (req, res) => {
     const id = req.params.id;
-    const body = req.body
+    const body = req.body;
 
-    Workout.findOneAndUpdate({ _id: id }, {$push:{ exercises: body }})
-    .then(data => {
-        console.log(data)
-        res.json(data);
-    })
-    .catch(err => {
-        res.status(400).json(err)
-    })
-});
+    Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: body } })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})
 
 router.post("/workouts", (req, res) => {
     Workout.create({})
-    .then(data => {
-        console.log(data)
-        res.json(data);
-    })
-    .catch(err => {
-        res.status(400).json(err)
-    })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 })
 
 router.get("/workouts/range", (req, res) => {
     Workout.find({}).limit(7)
-    .then(data => {
-        console.log(data)
-        res.json(data);
-    })
-    .catch(err => {
-        res.status(400).json(err)
-    })
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 })
-
 
 module.exports = router;
