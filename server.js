47b87592eaf1f5a77679476e0a57e7eb80667e1a
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2021-03-01 14:25:11 -0700

    fixed server.js seed issue

--- server.js
+++ server.js
@@ -2,30 +2,26 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3008;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
 { 
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true,
-}).then(() => console.log('connected to db'))
-.catch(err=> console.error('an error has occured', err));
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
-// routes
-app.use(require("./routes"));
-app.use(require("./routes/homeRoutes.js"));
+app.use(require("./routes"))
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
